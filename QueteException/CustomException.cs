6caca8abf22ec09416465d6a5d292585239941a2
siwar <siwarsaid8@gmail.com>
2024-06-11 11:41:06 +0200

    queteException

---
+++ QueteException/CustomException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueteException
+{
+    internal class CustomException : Exception
+    {
+        public DateTime Date { get; private set; }
+
+        public CustomException(string message) : base(message)
+        {
+            Date = DateTime.Now;
+        }
+    }
+}
---
+++ QueteException/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace QueteException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                ThrowError();
+            }
+            catch (CustomException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.Date);
+            }
+            static void ThrowError()
+            {
+                throw new CustomException("Houston ? On a un problème !");
+
+            }
+        }
+    }
+}
